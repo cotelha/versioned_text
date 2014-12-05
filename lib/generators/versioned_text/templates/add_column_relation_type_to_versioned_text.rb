d5dfaf2a3de920c9f0a126aae15cfecbcd45aa54
Guilherme Kuhn <g.kuhn0@gmail.com>
2014-12-05 19:19:55 -0200

    migration table name

--- lib/generators/versioned_text/templates/add_column_relation_type_to_versioned_text.rb
+++ lib/generators/versioned_text/templates/add_column_relation_type_to_versioned_text.rb
@@ -1,9 +1,11 @@
 class <%= migration_class_name %> < ActiveRecord::Migration
   def self.up
     add_column :version_texts, :relation_type,  :integer
+    add_column :version_text_versions, :relation_type, :integer
   end
 
   def self.down
     remove_column :version_texts, :relation_type
+    remove_column :version_text_versions, :relation_type
   end
 end
